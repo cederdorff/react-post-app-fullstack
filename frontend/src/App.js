cd94f4c67f377d3e80cab31e74bf0fefbe922c9c
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-04-30 10:32:34 +0200

    api request url changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Nav from "./components/Nav";
+import CreatePage from "./pages/CreatePage";
+import UpdatePage from "./pages/UpdatePage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <main>
+            <Nav />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/create" element={<CreatePage />} />
+                <Route path="/posts/:postId" element={<UpdatePage />} />
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </main>
+    );
 }
-
-export default App;
